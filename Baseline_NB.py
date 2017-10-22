ebab609925c43aca346a3041ce7573f50826d2cc
Amruta <hail.radical@gmail.com>
2017-10-21 23:08:21 -0600

    modified for 2.7

--- Baseline_NB.py
+++ Baseline_NB.py
@@ -2,13 +2,14 @@ import re
 import sys
 import math
 import nltk
+import io
 
 
 # Gives the number of reviews in the input document
 # marker is the start of review with ID-
 
 def count_n_c(input_document):
-    ip = open(input_document, 'r', encoding="utf8")
+    ip = io.open(input_document, 'r', encoding="utf8")
     all_tokens = ip.read()
     id_tokens = re.findall("ID-", all_tokens)
     count = len(id_tokens)
@@ -23,8 +24,8 @@ def log_prior_probability(input_pos, input_neg):
     pos = count_n_c(input_pos)
     neg = count_n_c(input_neg)
     n_doc = pos + neg
-    num1 = pos/n_doc
-    num2 = neg/n_doc
+    num1 = pos/float(n_doc)
+    num2 = neg/float(n_doc)
     log_prior_prob['input_pos'] = math.log10(num1)
     log_prior_prob['input_neg'] = math.log10(num2)
     return log_prior_prob
@@ -33,7 +34,7 @@ def log_prior_probability(input_pos, input_neg):
 # Gives the vocabulary in input document
 
 def vocabulary(input_document):
-    ip = open(input_document, 'r', encoding="utf8")
+    ip = io.open(input_document, 'r', encoding="utf8")
     all_tokens = ip.read()
     tokens = nltk.wordpunct_tokenize(all_tokens)
     words = [w.lower() for w in tokens]
@@ -56,7 +57,7 @@ def get_vocab_training(input_pos, input_neg):
 # Gives the bag of words in input_documents, returns iterable FreqDist
 
 def big_doc_c(input_document):
-    ip = open(input_document, 'r', encoding="utf8")
+    ip = io.open(input_document, 'r', encoding="utf8")
     all_tokens = ip.read()
     tokens = nltk.wordpunct_tokenize(all_tokens)
     words = [w.lower() for w in tokens]
@@ -92,8 +93,8 @@ def train_naive_bayes(input_pos, input_neg, smoothing_factor):
         log_probs = []
         count_pos = freq_dist_pos[word]
         count_neg = freq_dist_neg[word]
-        prob_pos = (count_pos + smoothing_factor) / (count_total_pos + v_alpha)
-        prob_neg = (count_neg + smoothing_factor) / (count_total_neg + v_alpha)
+        prob_pos = (count_pos + smoothing_factor) / float(count_total_pos + v_alpha)
+        prob_neg = (count_neg + smoothing_factor) / float(count_total_neg + v_alpha)
         log_prob_pos = math.log10(prob_pos)
         log_prob_neg = math.log10(prob_neg)
         log_probs.append(log_prob_pos)
@@ -110,7 +111,7 @@ def test_naive_bayes(input_pos, input_neg, test_doc, smoothing_factor):
     vocab_training = get_vocab_training(input_pos, input_neg)
     log_likelihood = train_naive_bayes(input_pos, input_neg, smoothing_factor)
     output = {}
-    test = open(test_doc, 'r', encoding="utf8")
+    test = io.open(test_doc, 'r', encoding="utf8")
     for line in test.readlines():
         if line != '\n':
             log_class = dict(log_prior)
@@ -131,10 +132,10 @@ def test_naive_bayes(input_pos, input_neg, test_doc, smoothing_factor):
 
 def get_gold_std(train_pos, train_neg):
     gold_set = {}
-    test = open(train_pos, 'r', encoding="utf8")
+    test = io.open(train_pos, 'r', encoding="utf8")
     for line in test.readlines():
         gold_set[line.split(None, 1)[0]] = 'POS'
-    test = open(train_neg, 'r', encoding="utf8")
+    test = io.open(train_neg, 'r', encoding="utf8")
     for line in test.readlines():
         gold_set[line.split(None, 1)[0]] = 'NEG'
     return gold_set
@@ -148,7 +149,7 @@ def give_accuracy(train_pos, train_neg, test_output):
     for elem in test_output:
         if test_output[elem] == gold_set[elem]:
             count = count + 1
-    return count / len(test_output)
+    return count / float(len(test_output))
 
 
 if __name__ == "__main__":
